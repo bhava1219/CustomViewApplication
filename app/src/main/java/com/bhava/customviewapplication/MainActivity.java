808e0bd3eef3ed08a40b758d7f3410831ece1c27
Bhavadhaarany <bhavadhaarany12@gmail.com>
2020-04-05 18:17:37 +0530

    Initial commit

---
+++ app/src/main/java/com/bhava/customviewapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.bhava.customviewapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
+    private CustomView shapes;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        shapes = findViewById(R.id.shapes);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),shapes.getSelectedShape(),Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
